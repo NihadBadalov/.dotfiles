530a501c6518e908e5e3483cc8abb1e19d44e63a
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-03-26 14:08:07 +0100

    nvim: change lsp complete() keybind

--- nvim/.config/nvim/lua/nihad/lazy/lsp.lua
+++ nvim/.config/nvim/lua/nihad/lazy/lsp.lua
@@ -91,7 +91,7 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-B>"] = cmp.mapping.complete(),
         ["<C-u>"] = cmp.mapping.scroll_docs(-3),
         ["<C-d>"] = cmp.mapping.scroll_docs(3),
         ['<C-g>'] = cmp.mapping.abort(),
