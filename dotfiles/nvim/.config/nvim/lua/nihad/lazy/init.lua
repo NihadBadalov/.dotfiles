94964c9c2dcebeaaa218922bb74595bfb528b92f
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-12 23:02:01 +0000

    chore: remove merging text

--- dotfiles/nvim/.config/nvim/lua/nihad/lazy/init.lua
+++ dotfiles/nvim/.config/nvim/lua/nihad/lazy/init.lua
@@ -31,23 +31,6 @@ return {
       'ldelossa/litee.nvim'
     }
   },
-<<<<<<< HEAD:dotfiles/nvim/.config/nvim/lua/nihad/lazy/init.lua
-  -- vim-tmux-navigator
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-    },
-  },
-=======
->>>>>>> f38fa0536c0ead32ae4f59bb831fc07876081fdb:nvim/.config/nvim/lua/nihad/lazy/init.lua
-
   "eandrju/cellular-automaton.nvim",
   "gpanders/editorconfig.nvim",
 
