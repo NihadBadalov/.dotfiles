a9a03a670e9af8bba55d1f42c15f88ce768c266e
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-15 22:18:38 +0000

    nvim: remove codeium. peace

--- dotfiles/nvim/.config/nvim/lua/nihad/lazy/init.lua
+++ dotfiles/nvim/.config/nvim/lua/nihad/lazy/init.lua
@@ -13,18 +13,18 @@ return {
   {
     "github/copilot.vim",
   },
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({
-        -- https://github.com/Exafunction/codeium.vim
-      })
-    end
-  },
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   config = function()
+  --     require("codeium").setup({
+  --       -- https://github.com/Exafunction/codeium.vim
+  --     })
+  --   end
+  -- },
   {
     'ldelossa/gh.nvim',
     dependencies = {
