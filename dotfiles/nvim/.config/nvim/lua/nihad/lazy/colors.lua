dcafde777c61e0241bf718ad3bd8ca634445cce7
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-21 22:58:34 +0200

    nvim: more contrast colors

--- dotfiles/nvim/.config/nvim/lua/nihad/lazy/colors.lua
+++ dotfiles/nvim/.config/nvim/lua/nihad/lazy/colors.lua
@@ -2,6 +2,9 @@ function ColorMyPencils(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
   -- Shift+k, supposedly
   -- we don't need er
   -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
@@ -39,7 +42,10 @@ return {
     name = "rose-pine",
     config = function()
       require('rose-pine').setup({
-        disable_background = false,
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
       })
 
       vim.cmd("colorscheme rose-pine")
