5edb350d93347d28d1414331bafb978b81f81931
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-08-31 12:35:01 +0000

    nvim: two more keybinds
    - `<C-e>` - close window on the bottom
    - `<leader>ge` - quick go error handling if-statement

--- dotfiles/nvim/.config/nvim/lua/nihad/remap.lua
+++ dotfiles/nvim/.config/nvim/lua/nihad/remap.lua
@@ -16,10 +16,10 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>vwm", function()
-  require("vim-with-me").StartVimWithMe()
+	require("vim-with-me").StartVimWithMe()
 end)
 vim.keymap.set("n", "<leader>svwm", function()
-  require("vim-with-me").StopVimWithMe()
+	require("vim-with-me").StopVimWithMe()
 end)
 
 vim.keymap.set("n", "<leader>vs", "<C-w>v")
@@ -48,15 +48,17 @@ vim.keymap.set("n", "<leader>mj", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("n", "<leader>ge", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
+	vim.cmd("so")
 end)
 
 vim.keymap.set("n", "zn", "$viBhzf", {
-  desc = "Fold { or ( block",
+	desc = "Fold { or ( block",
 })
 
 vim.keymap.set("n", "ga", "<cmd>EasyAlign<CR>")
@@ -69,3 +71,6 @@ vim.keymap.set("v", ">", ">gv")
 -- Indent with tab
 vim.keymap.set("v", "<Tab>", ">gv")
 vim.keymap.set("v", "<S-Tab>", "<gv")
+
+-- Close window on the bottom
+vim.keymap.set("n", "<C-w>e", "<C-w>j<C-w>q")
